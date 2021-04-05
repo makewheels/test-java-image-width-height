e1e576ac29e441a66df0f951d8f9da254802299e
bq <email>
2021-04-05 21:39:02 +0800

    Game Over

---
+++ src/main/java/com/example/testjavaimagewidthheight/RunTest.java
@@ -0,0 +1,22 @@
+package com.example.testjavaimagewidthheight;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class RunTest {
+    public static void main(String[] args) {
+        File image = new File("D:\\etc\\WIN_Pro.jpg");
+        BufferedImage bufferedImage = null;
+        try {
+            bufferedImage = ImageIO.read(image);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int width = bufferedImage.getWidth();
+        int height = bufferedImage.getHeight();
+        System.out.println(width);
+        System.out.println(height);
+    }
+}
