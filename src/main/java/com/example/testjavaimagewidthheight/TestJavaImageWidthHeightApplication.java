d7aaa32d1f24bac15e15957d065b368b550f31e5
bq <email>
2021-04-05 21:36:14 +0800

    init

---
+++ src/main/java/com/example/testjavaimagewidthheight/TestJavaImageWidthHeightApplication.java
@@ -0,0 +1,13 @@
+package com.example.testjavaimagewidthheight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJavaImageWidthHeightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestJavaImageWidthHeightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testjavaimagewidthheight/TestJavaImageWidthHeightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testjavaimagewidthheight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJavaImageWidthHeightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
